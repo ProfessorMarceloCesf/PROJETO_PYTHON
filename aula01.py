8287205da7b52b87661f91907e304ed08022aa5f
professor Marcelo <avalia.cesf.marcelo@gmail.com>
2024-03-27 10:40:03 -0300

    Subir aula01

---
+++ aula01.py
@@ -0,0 +1,10 @@
+import os
+
+#print("Olá mundo cruel")
+print("O 3DS é a turma número 1 do Sagrada")
+turma="2DS"
+pos=2
+print("o",turma,"é a turma número",pos,"do Sagrada")
+print(type(turma))
+print(type(pos))
+#os.system("cls")
